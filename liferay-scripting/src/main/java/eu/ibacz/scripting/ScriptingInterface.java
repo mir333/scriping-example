ef5256f3812217400c4aa5c7a4432d53cd356f3d
miroslav.ligas <miroslav.ligas@ibacz.eu>
2013-11-28 17:31:25 +0100

    script container bean

---
+++ liferay-scripting/src/main/java/eu/ibacz/scripting/ScriptingInterface.java
@@ -0,0 +1,15 @@
+/* ===========================================================================
+ * IBA CZ Confidential
+ *
+ * (c) Copyright IBA CZ 2011 ALL RIGHTS RESERVED
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets.
+ *
+ * =========================================================================== */
+package eu.ibacz.scripting;
+
+/**
+ * @author Miroslav Ligas <miroslav.ligas@ibacz.eu>
+ */
+public interface ScriptingInterface {
+}
---
+++ liferay-scripting/src/main/java/eu/ibacz/scripting/impl/ScriptingInterfaceImpl.java
@@ -0,0 +1,17 @@
+/* ===========================================================================
+ * IBA CZ Confidential
+ *
+ * (c) Copyright IBA CZ 2011 ALL RIGHTS RESERVED
+ * The source code for this program is not published or otherwise
+ * divested of its trade secrets.
+ *
+ * =========================================================================== */
+package eu.ibacz.scripting.impl;
+
+import eu.ibacz.scripting.ScriptingInterface;
+
+/**
+ * @author Miroslav Ligas <miroslav.ligas@ibacz.eu>
+ */
+public class ScriptingInterfaceImpl implements ScriptingInterface {
+}
