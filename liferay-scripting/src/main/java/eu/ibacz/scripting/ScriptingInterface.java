fae8db79135a99cb9d561cce0a821095c36de2ca
miroslav.ligas <miroslav.ligas@ibacz.eu>
2013-11-29 14:34:06 +0100

    wiki pages sorter

--- liferay-scripting/src/main/java/eu/ibacz/scripting/ScriptingInterface.java
+++ liferay-scripting/src/main/java/eu/ibacz/scripting/ScriptingInterface.java
@@ -8,8 +8,20 @@
  * =========================================================================== */
 package eu.ibacz.scripting;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portlet.wiki.model.WikiPage;
+import com.liferay.portlet.wiki.service.WikiPageLocalServiceUtil;
+
+import java.util.List;
+
 /**
  * @author Miroslav Ligas <miroslav.ligas@ibacz.eu>
  */
 public interface ScriptingInterface {
+
+    String test();
+
+    public void reorganizeWiki() throws SystemException, PortalException;
 }
--- liferay-scripting/src/main/java/eu/ibacz/scripting/impl/ScriptingInterfaceImpl.java
+++ liferay-scripting/src/main/java/eu/ibacz/scripting/impl/ScriptingInterfaceImpl.java
@@ -8,10 +8,65 @@
  * =========================================================================== */
 package eu.ibacz.scripting.impl;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portlet.wiki.model.WikiPage;
+import com.liferay.portlet.wiki.service.WikiPageLocalServiceUtil;
 import eu.ibacz.scripting.ScriptingInterface;
+
+import java.util.List;
 
 /**
  * @author Miroslav Ligas <miroslav.ligas@ibacz.eu>
  */
 public class ScriptingInterfaceImpl implements ScriptingInterface {
+
+    @Override
+    public void reorganizeWiki() throws SystemException, PortalException {
+        Long userId = 10196l;
+        Long nodeId = 11730l;
+        String content = "test";
+        String rootTitle = "FrontPage";
+
+        List<WikiPage> pages = WikiPageLocalServiceUtil.getPages(nodeId, -1, -1);
+        for (WikiPage page : pages) {
+            String title = page.getTitle();
+
+            if (!rootTitle.equals(title) && title.length() > 1 && getParentTitleSize(page) > 1) {
+
+                char firstLetter = title.charAt(0);
+                String categoryTitle = String.valueOf(firstLetter).toUpperCase();
+                List<WikiPage> category = WikiPageLocalServiceUtil.getPages(nodeId, categoryTitle, 0, 1);
+                if (category != null && !category.isEmpty()) {
+                    page.setParentTitle(category.get(0).getTitle());
+                    WikiPageLocalServiceUtil.updateWikiPage(page);
+                } else {
+                    ServiceContext context = new ServiceContext();
+                    context.setAddGuestPermissions(true);
+                    WikiPage wikiPage = WikiPageLocalServiceUtil.addPage(userId, nodeId, categoryTitle, 1, content, null, false, "creole", true, rootTitle, null, context);
+                    wikiPage.setStatus(WorkflowConstants.STATUS_APPROVED);
+                    WikiPageLocalServiceUtil.updateWikiPage(wikiPage);
+                    page.setParentTitle(categoryTitle);
+                    WikiPageLocalServiceUtil.updateWikiPage(page);
+                }
+            }
+        }
+    }
+
+    private int getParentTitleSize(WikiPage page) {
+        int result = 0;
+        String parentTitle = page.getParentTitle();
+
+        if (parentTitle != null) {
+            result = parentTitle.length();
+        }
+
+        return result;
+    }
+
+    public String test() {
+        return "Hallo world!";
+    }
 }
